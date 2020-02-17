7843fcfacc4f13c1e6cd29d1b2b343c8d5c3bd08
validcoderu <validcoderu@gmail.com>
2020-02-17 15:49:59 +0100

    Первое задание учебного проекта

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
 var ads = [];
-var offer_titles = ['Заголовок предложения 1', 'Заголовок предложения 2', 'Заголовок предложения 3', 'Заголовок предложения 4', 'Заголовок предложения 5', 'Заголовок предложения 6', 'Заголовок предложения 7', 'Заголовок предложения 8'];
-var offer_addreses = ['200, 130', '300, 185', '400, 250', '500, 350', '600, 450', '700, 350', '800, 250'];
-var offer_prices = ['1500', '200', '2700', '800', '999', '8900', '1000', '9999'];
-var offer_types = ['palace', 'flat', 'house', 'bungalo'];
-var offer_rooms = ['1', '2', '3', '4'];
-var offer_guests = ['1', '2', '3', '4', '5', '6', '7', '8'];
-var offer_checkins = ['12:00', '13:00', '14:00'];
-var offer_checkouts = ['12:00', '13:00', '14:00'];
-var offer_feutures = [['washer', 'elevator', 'conditioner'], ['wifi', 'dishwasher', 'parking'], ['wifi', 'parking', 'washer',  'conditioner']];
-var offer_descriptions = ['Строка с описанием 1', 'Строка с описанием 2', 'Строка с описанием 3', 'Строка с описанием 4', 'Строка с описанием 5', 'Строка с описанием 6', 'Строка с описанием 7', 'Строка с описанием 8'];
-var offer_photos = [['http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg']];
-var offer_location_x = ['200', '300', '400', '500', '600', '700', '800', '630'];
-var offer_location_y = ['130', '200', '270', '340', '430', '500', '570', '630'];
+var OFFER_TITLES = ['Заголовок предложения 1', 'Заголовок предложения 2', 'Заголовок предложения 3', 'Заголовок предложения 4', 'Заголовок предложения 5', 'Заголовок предложения 6', 'Заголовок предложения 7', 'Заголовок предложения 8'];
+var OFFER_ADDRESES = ['200, 130', '300, 185', '400, 250', '500, 350', '600, 450', '700, 350', '800, 250'];
+var OFFER_PRICES = ['1500', '200', '2700', '800', '999', '8900', '1000', '9999'];
+var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var OFFER_ROOMS = ['1', '2', '3', '4'];
+var OFFER_GUESTS = ['1', '2', '3', '4', '5', '6', '7', '8'];
+var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
+var OFFER_CHECKOUTS = ['12:00', '13:00', '14:00'];
+var OFFER_FEUTURES = [['washer', 'elevator', 'conditioner'], ['wifi', 'dishwasher', 'parking'], ['wifi', 'parking', 'washer', 'conditioner']];
+var OFFER_DESCRIPTIONS = ['Строка с описанием 1', 'Строка с описанием 2', 'Строка с описанием 3', 'Строка с описанием 4', 'Строка с описанием 5', 'Строка с описанием 6', 'Строка с описанием 7', 'Строка с описанием 8'];
+var OFFER_PHOTOS = [['http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg']];
+var OFFER_LOCATION_X = ['200', '300', '400', '500', '600', '700', '800', '630'];
+var OFFER_LOCATION_Y = ['130', '200', '270', '340', '430', '500', '570', '630'];
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -33,25 +33,25 @@ var getRandomValue = function (data) {
 var generateAds = function () {
   for (var i = 0; i < ADS_QUANTITY; i++) {
     var ad = {
-      "author": {
-        "avatar": "img/avatars/user0" + [i + 1] + ".png"
+      'author': {
+        'avatar': 'img/avatars/user0' + [i + 1] + '.png'
       },
-      "offer": {
-        "title": getRandomValue(offer_titles),
-        "address": getRandomValue(offer_addreses),
-        "price": getRandomValue(offer_prices),
-        "type": getRandomValue(offer_types),
-        "rooms": getRandomValue(offer_rooms),
-        "guests": getRandomValue(offer_guests),
-        "checkin": getRandomValue(offer_checkins),
-        "checkout": getRandomValue(offer_checkouts),
-        "features": getRandomValue(offer_feutures),
-        "description": getRandomValue(offer_descriptions),
-        "photos": getRandomValue(offer_photos)
+      'offer': {
+        'title': getRandomValue(OFFER_TITLES),
+        'address': getRandomValue(OFFER_ADDRESES),
+        'price': getRandomValue(OFFER_PRICES),
+        'type': getRandomValue(OFFER_TYPES),
+        'rooms': getRandomValue(OFFER_ROOMS),
+        'guests': getRandomValue(OFFER_GUESTS),
+        'checkin': getRandomValue(OFFER_CHECKINS),
+        'checkout': getRandomValue(OFFER_CHECKOUTS),
+        'features': getRandomValue(OFFER_FEUTURES),
+        'description': getRandomValue(OFFER_DESCRIPTIONS),
+        'photos': getRandomValue(OFFER_PHOTOS)
       },
-      "location": {
-        "x": getRandomValue(offer_location_x),
-        "y": getRandomValue(offer_location_y)
+      'location': {
+        'x': getRandomValue(OFFER_LOCATION_X),
+        'y': getRandomValue(OFFER_LOCATION_Y)
       }
     };
 
